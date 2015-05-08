524c025e4ce3269a1ea41449328a7f0bf165b6aa
Justin Sternberg <justin@dsgnwrks.pro>
2015-05-07 23:02:34 -0400

    Fix wp standards issues

--- include/config.php
+++ include/config.php
@@ -2,7 +2,7 @@
 /**
  * Remember plugin path & URL
  */
-define( 'MUCD_PATH', plugin_basename( realpath( dirname( __FILE__ ).'/..') ) );
+define( 'MUCD_PATH', plugin_basename( realpath( dirname( __FILE__ ).'/..' ) ) );
 define( 'MUCD_COMPLETE_PATH', WP_PLUGIN_DIR.'/'.MUCD_PATH );
 define( 'MUCD_URL', WP_PLUGIN_URL.'/'.MUCD_PATH );
 
